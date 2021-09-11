daa001e0c6db80ab50ab8ce87e9a170c52dfcd78
magal1337 <lucas.m.e.ferreira@gmail.com>
2021-09-11 13:06:49 -0300

    Add env temperature variable

--- metodos_numericos/lista_1/lista_1_finite_diff_method.py
+++ metodos_numericos/lista_1/lista_1_finite_diff_method.py
@@ -12,6 +12,7 @@ t_0 = 180
 t_n = 65
 x_0 = 0
 x_n = 0.15
+t_amb = 20
 h_conv = 150
 d = 3.5 * 10**(-3)
 k = 120
@@ -19,8 +20,8 @@ k = 120
 class finite_diff_heat_problem:
     """
     Solve EDO problem using finite difference method for a heat transfer problem with the pattern:
-    y" - m^2(y - T_amb)
-    Where m and T_amb are constant variables.
+    y" - m^2(y - t_amb)
+    Where m and t_amb are constant variables.
     Args:
         node: number of nodes to use for problem solver.
         t_0: Initial Temperature for boundary condition.
@@ -29,20 +30,21 @@ class finite_diff_heat_problem:
         x_n: Final Position for boundary condition.
         h_conv: Trade heat by convexion coef.
         d: Fin Diameter.
-        k: Fin = Thrtmsl conductivity.
+        k: Fin = Thermal conductivity.
+        t_amb: Environment Temperature.
     Returns:
         matrix_a: The A Transformation matrix for Ax = b.
         matrix_b: The b Result matrix for Ax = b.
         y_solution: The solution for the problem Ax = b.
     """
 
-    def __init__(self,node,t_0,t_n,x_0,x_n,h_conv,d,k):
+    def __init__(self,node,t_0,t_n,x_0,x_n,h_conv,d,k,t_amb):
         self.p = np.pi * d
         self.a = (np.pi*d*d) / 4
         self.m = (h_conv*self.p) / (k*self.a)
         self.h = (x_n - x_0) / node
         self.Q = 2 + (self.m*self.h*self.h)
-        self.W = 20*self.m*self.h*self.h
+        self.W = t_amb*self.m*self.h*self.h
         self.t_0 = t_0
         self.t_n = t_n
         self.x_0 = x_0
@@ -84,7 +86,7 @@ class finite_diff_heat_problem:
         plt.show()
 
 logging.info("Initializing problem, loading variables...")
-heat_problem = finite_diff_heat_problem(node,t_0,t_n,x_0,x_n,h_conv,d,k)
+heat_problem = finite_diff_heat_problem(node,t_0,t_n,x_0,x_n,h_conv,d,k,t_amb)
 matrix_a, matrix_b = heat_problem.create_matrix_for_linear_transform()
 y_solution = heat_problem.solve_linear_problem(matrix_a, matrix_b)
 heat_problem.plot_results(y_solution)
