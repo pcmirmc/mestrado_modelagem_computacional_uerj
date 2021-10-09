7b53c79891d7ca1a88a8ba2d2277930085db2c9f
magal1337 <lucas.m.e.ferreira@gmail.com>
2021-10-09 11:41:21 -0300

    Adding comments and fixes to T1

--- metodos_numericos/trabalho_1/main.py
+++ metodos_numericos/trabalho_1/main.py
@@ -3,29 +3,43 @@ import random
 from root_find import rootFind
 import logging
 import coloredlogs
-
+#Habilitando logs
 coloredlogs.install()
-
 logging.basicConfig(level=logging.INFO) 
 
 if __name__ == "__main__":
-
-    method_choiced = input('Escolha 1 para o metodo de newton ou 2 para o metodo da bissecção.')
+    # Escolhendo metodo 1 para newton e 2 para bisseccao
+    method_choiced = input('Escolha 1 para o metodo de newton ou 2 para o metodo da bissecção:')
+    h_choiced = input('Digite o valor para h2:')
+    #calculo das assintotas verticais para k = {0,1,2,3,4,5}
     x_lim = [(np.pi/2) + (k*np.pi) for k in [0,1,2,3,4,5]]
+    # criacao dos pares usando as assintotas verticais e armazenando em um dicionario
     dict ={}
     for a in range(0,6):
         if a==0:
             dict[f'par_{a+1}'] = (0,x_lim[a]) 
         else: 
             dict[f'par_{a+1}'] = (x_lim[a-1],x_lim[a])
+    # iniciando a busca das raizes para cada par (total 6 pares)
     i=1
     for k,value in dict.items():
         logging.info(f"INICIANDO A BUSCA DA RAIZ {i}...")
-        random_x_ini = random.uniform(value[0],value[1])
-        problem = rootFind(random_x_ini,0.000001,1)
+        # valor inicial para x0 usando a media de cada par
+        if float(h_choiced) < 10:
+            selected_x_ini = (value[0] + value[1]) / 2
+        else:
+            # para valores altos de h2 usar um valor proximo a assintota vertical mais a direita para valor inicial x0
+            selected_x_ini = value[1] - 0.0001
+        #instanciando o metodo para achar as raizes no arquiavo root_find.py, tolerancia de 10^-6, valor de h2 para esse exemplo valor 1
+        problem = rootFind(selected_x_ini,0.000001,float(h_choiced))
+        #rodando o metodo escolhido pelo usuario
         if method_choiced == '1':
             problem.newton_raphson_method()
         else:
-            problem.bisection_method(value[0]+0.00001,value[1])
+            if k == 'par_1':
+                problem.bisection_method(value[0],value[1])
+            else:
+                problem.bisection_method(value[0]+0.000001,value[1]-0.000001)
+        # termino do loop raiz encontrada, proxima iteracao sera escolhida o proximo par para obtencao da proxima raiz
         logging.info(f"BUSCA DA RAIZ {i} FINALIZADA!!!")
         i+=1
--- metodos_numericos/trabalho_1/root_find.py
+++ metodos_numericos/trabalho_1/root_find.py
@@ -2,50 +2,69 @@ from types import FunctionType
 import numpy as np
 import logging
 
-
+#criacao de classe que roda os metodos de newton e bisseccao
 class rootFind:
+    """
+    Roda os metodos numericos de newton e bisseccao para uma funcao do tipo f(x) = xtan(x) - h. Ambos os metodos foi-se utilizado a recursividade de funcoes para a geracao de cada iteracao.
+    Args:
+        x_ini: O valor inicial x0 para o metodo de newton.
+        tol: Tolerancia a ser batida para ambos os metodos.
+        h: Termo Independente h2 da funcao.
+        i: Variavel auxiliar para iteracao do algoritmo.
+    Returns:
+        Resultados de cada iteracao seja do metodo de newton ou do metodo da bissecao.
+    """
     def __init__(self,x_ini:float,tol:float,h:float,i=1):
+        #instanciando variaveis para serem usadas em todas as funcoes da classe
         self.x_ini = x_ini
         self.tol = tol
         self.h =h
         self.i = i
 
     def tan_function(self,beta=None)->float:
+        # funcao tangente target f(x)= xtan(x) - h
         if beta != None:
             return (beta * np.tan(beta)) - self.h
         else:
             return (self.x_ini * np.tan(self.x_ini)) - self.h
 
     def dev_tan_function(self)->float:
+        # funcao que calcula a derivada da funcao tan_function
         return (self.x_ini * (1/np.cos(self.x_ini))**2) + np.tan(self.x_ini)
 
     def next_xi(self)->float:
+        # calcula o proximo xi do metodo de newton
         xi = self.x_ini - (self.tan_function() / self.dev_tan_function())
         return xi
 
     def newton_raphson_method(self)->None:
+        #roda o metodo de newton
         logging.info(f"Iniciando Iteracao {self.i} do metodo de Newton...")
         y = self.tan_function()
+        # verifica criterio de tolerancia
         if abs(y) <= self.tol:
             return logging.info(f"Solucao encontrada na iteracao {self.i}, valor aproximado para beta e: {self.x_ini} com um erro de {abs(y)}.")
         else:
-            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, indo para a proxima iteracao...")
+            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, valor aproximado para beta e: {self.x_ini}, indo para a proxima iteracao...")
             self.i +=1
             self.x_ini = self.next_xi()
             return self.newton_raphson_method()
 
     def bisection_method(self,a:float, b:float)->None: 
+        #roda o metodo da bisseccao
         logging.info(f"Iniciando Iteracao {self.i} do metodo da bissecção...")
+        #novo valor de m para o intervalo dado
         m = (a + b)/2
+        # verifica o criterio de tolerancia
         if np.abs(self.tan_function(beta=m)) < self.tol:
             return logging.info(f"Solucao encontrada na iteracao {self.i}, valor aproximado para beta e: {m} com um erro de {abs(self.tan_function(beta=m))}.")
-
+        # verifica se a saida de m e igual a saida do extremo a do intervalo, se for m sera usado no lugar de a na proxima iteracao.
         elif np.sign(self.tan_function(beta=a)) == np.sign(self.tan_function(beta=m)):
-            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, indo para a proxima iteracao...")
+            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, valor aproximado para beta e: {m}, indo para a proxima iteracao...")
             self.i+=1
             return self.bisection_method(m, b)
-
+        # verifica se a saida de m e igual a saida do extremo b do intervalo, se for, m sera usado no lugar de b na proxima iteracao.
         elif np.sign(self.tan_function(beta=b)) == np.sign(self.tan_function(beta=m)):
-            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, indo para a proxima iteracao...")
+            logging.warning(f"Solucao nao encontrada na iteracao {self.i}, valor aproximado para beta e: {m}, indo para a proxima iteracao...")
             self.i+=1
             return self.bisection_method(a, m)
